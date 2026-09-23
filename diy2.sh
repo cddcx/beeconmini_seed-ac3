42768e417e0b3368f23d2cea672a3f100f558cae
cddcx <84568016+cddcx@users.noreply.github.com>
2026-09-23 14:24:18 +0800

    Comment out wan port LED modification command
    Comment out the sed command for modifying wan port LED.

--- diy2.sh
+++ diy2.sh
@@ -44,7 +44,7 @@ echo "========================="
 sed -i 's/root:::0:99999:7:::/root:$1$SOP5eWTA$fJV8ty3QohO0chErhlxCm1:18775:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 ##修改wan口灯
-sed -i 's/13 GPIO_ACTIVE_LOW/13 GPIO_ACTIVE_HIGH/g' target/linux/mediatek/dts/mt7986a-beeconmini-seed-ac3.dts
+#sed -i 's/13 GPIO_ACTIVE_LOW/13 GPIO_ACTIVE_HIGH/g' target/linux/mediatek/dts/mt7986a-beeconmini-seed-ac3.dts
 
 ## 修改target.mk
 sed -i 's/dnsmasq/dnsmasq-full/g' include/target.mk

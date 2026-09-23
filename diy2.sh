27af9cabe09727cd41226d63c3ca031df52ed764
cddcx <84568016+cddcx@users.noreply.github.com>
2026-09-23 12:24:36 +0800

    Modify WAN LED settings and password in diy2.sh
    Update WAN port LED configuration and modify password.

--- diy2.sh
+++ diy2.sh
@@ -43,6 +43,9 @@ echo "========================="
 # 修改密码
 sed -i 's/root:::0:99999:7:::/root:$1$SOP5eWTA$fJV8ty3QohO0chErhlxCm1:18775:0:99999:7:::/g' package/base-files/files/etc/shadow
 
+##修改wan口灯
+sed -i 's/13 GPIO_ACTIVE_LOW/13 GPIO_ACTIVE_HIGH/g' target/linux/mediatek/dts/mt7986a-beeconmini-seed-ac3.dts
+
 ## 修改target.mk
 sed -i 's/dnsmasq/dnsmasq-full/g' include/target.mk
 sed -i "s/kmod-nft-offload/kmod-nft-offload kmod-nft-tproxy/" include/target.mk
